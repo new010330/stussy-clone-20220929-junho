446054a302cb3f929e34400e401eef230831dcda
new010330 <rlarudtjs74a@naver.com>
2022-09-29 13:04:09 +0900

    stussy site(CloneCoding) header

---
+++ src/main/java/com/stussy/stussclone20220929junho/StussClone20220929JunhoApplication.java
@@ -0,0 +1,13 @@
+package com.stussy.stussclone20220929junho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussClone20220929JunhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StussClone20220929JunhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stussy/stussclone20220929junho/StussClone20220929JunhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stussy.stussclone20220929junho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StussClone20220929JunhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
